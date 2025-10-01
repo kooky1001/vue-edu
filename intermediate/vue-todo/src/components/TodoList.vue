03ff8e132914b4c8f7b4cbbd947e0d872b073673
kooky1001 <kooky1001@gmail.com>
2025-10-01 17:14:17 +0900

    리스트 트렌지션 적용

--- intermediate/vue-todo/src/components/TodoList.vue
+++ intermediate/vue-todo/src/components/TodoList.vue
@@ -1,23 +1,25 @@
 <template>
   <v-card>
     <v-list>
-      <v-list-item-group>
-        <v-list-item v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" v-bind:class="{textCompletedLine: todoItem.completed}">
-          <v-row>
-            <v-col cols="1">
-              <v-checkbox v-bind:input-value="todoItem.completed" v-on:click="completeTodoItem(todoItem)"/>
-            </v-col>
-            <v-col cols="10" class="text-center my-5">
-              <v-input v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</v-input>
-            </v-col>
-            <v-col cols="1">
-              <v-btn class="ml-3" large color="warning" width="80%" v-on:click="removeTodoItem(todoItem, index)">
-                <v-icon large>mdi-trash-can-outline</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-list-item>
-      </v-list-item-group>
+        <v-list-item-group>
+      <transition-group name="list" tag="p">
+          <v-list-item v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" v-bind:class="{textCompletedLine: todoItem.completed}">
+            <v-row>
+              <v-col cols="1">
+                <v-checkbox v-bind:input-value="todoItem.completed" v-on:click="completeTodoItem(todoItem)"/>
+              </v-col>
+              <v-col cols="10" class="text-center my-5">
+                <v-input v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</v-input>
+              </v-col>
+              <v-col cols="1">
+                <v-btn class="ml-3" large color="warning" width="80%" v-on:click="removeTodoItem(todoItem, index)">
+                  <v-icon large>mdi-trash-can-outline</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-list-item>
+      </transition-group>
+        </v-list-item-group>
     </v-list>
   </v-card>
 </template>
@@ -42,7 +44,11 @@ export default {
   text-decoration: line-through;
 }
 
-.textCompletedLine {
-  //background-color: blue;
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
