0066244df6250ae5d5b1e42106afc1e9ce616d5b
kooky1001 <kooky1001@gmail.com>
2025-10-01 16:59:31 +0900

    모달 컴포넌트 등록

--- intermediate/vue-todo/src/components/TodoInput.vue
+++ intermediate/vue-todo/src/components/TodoInput.vue
@@ -10,21 +10,42 @@
         </v-btn>
       </v-col>
     </v-row>
+    <CommonModal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고!
+      </h3>
+      <div slot="body">
+        아무것도 입력하지 않았습니다.
+      </div>
+      <div slot="footer">
+        <button class="modal-default-button" @click="showModal = false">
+          OK
+        </button>
+      </div>
+    </CommonModal>
   </div>
 </template>
 
 <script>
+import CommonModal from "@/components/common/CommonModal.vue";
+
 export default {
   name: "TodoInput",
+  components: {CommonModal},
   data: function() {
     return {
       newTodoItem: "",
+      showModal: false,
     }
   },
   methods: {
     addTodoItem: function() {
-      this.$emit('addItem', this.newTodoItem);
-      this.clearTodoItem();
+      if (this.newTodoItem !== '') {
+        this.$emit('addItem', this.newTodoItem);
+        this.clearTodoItem();
+      } else {
+        this.showModal = !this.showModal;
+      }
     },
     clearTodoItem: function() {
       this.newTodoItem = "";
